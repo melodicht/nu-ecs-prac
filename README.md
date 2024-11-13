a0868c7003f153c97c083cd51a1bce0f6bcc139e
melodicht <marvincht@gmail.com>
2024-11-13 18:51:45 -0500

    Change existing implementation to follow tutorial 100%, and add build.sh

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Placeholder
+## Design Notes
+
+- The reason why `u64` is used for EntityID is to avoid narrowing. We use
+  `std::vector::size` for getting unique EntityIDs, which outputs `u64`.
--- main.cpp
+++ main.cpp
@@ -1,64 +1,84 @@
 #include <iostream>
+#include <vector>
+#include <bitset>
 
 // for the ball game we make with the ECS
 #define BALL_RADIUS 10
 
-typedef uint16_t EntityID;
-
-class Component {}
+#define u32 uint32_t
+#define u64 uint64_t
 
 //////////////// COMPONENTS ////////////////
+
+// Gets a unique identifier for the given component T, which is
+// guaranteed to be different from previous outputs for only the first
+// 2^64 unsigned ints. 
+u64 s_componentCounter;
+template <class T>
+int GetId()
+{
+  static u64 s_componentId = s_componentCounter++;
+  return s_componentId;
+}
+/*
+ * Examples
+ * GetId<TransformComponent>() -> 0
+ * GetId<TransformComponent>() -> 0
+ * GetId<BallPhysics>() -> 1
+ * GetId<TransformComponent>() -> 0
+ * GetId<BallPhysics>() -> 1
+ */
+
 // Represents a point in a 2D space as a Component
-class TransformComponent : Component {
+class TransformComponent {
     float x_pos;
     float y_pos;
-}
-class BallPhysics : Component {
-    
-}
-// A Scene is a collection of Entities and Components
-class Scene {
-    // an Entity Entry that describes the ID and Components it uses
-    struct EntityEntry {
-        EntityID id;
-        std::vector<Component*> components;
-    }
-
-    std::vector<EntityEntry> entities;
-    std::vector<Component> component;
-    EntityID next_id = 0;
+};
 
 
-    // Creates a new Entity in this scene and 
-    EntityID NewEntity() {
-        entities.push_back({ next_id, ComponentMask() });
-        next_id++;
-        return entities.back().id;
-    }
 
-    // GetComponent : Gets a component, under the assumption that a object cannot
-    //                have more than one component of the same type.
+/*
+ * TYPE DEFINITIONS AND CONSTANTS
+ */
 
-    // Creates a Component from the given typename and assigns it to an Entity
-    // identified by the given EntityID. Typename MUST be a Component
-    template<typename T>
-    Component* AssignNewComponent(EntityID id) {
-        T* new_comp = new T();
-        if (!std::is_base_of_v<Component, T>) {
-            std::cout << "BAD BAD BAD BAD\n";
-        }
-        components.push_back(&new_comp);
-        entities[id].components.push_back(component);
-        return new_comp;
-    }
-}
+typedef u64 EntityID;
+const u32 MAX_COMPONENTS = 32;
+typedef std::bitset<MAX_COMPONENTS> ComponentMask;
 
+/*
+ * SCENE DEFINITION + FUNCTIONALITY
+ */
 
+struct Scene {
+  struct EntityEntry {
+    EntityID id;  // though redundent with index in vector, required
+                  // for deleting entities,
+    ComponentMask mask;
+  };
+  std::vector<EntityEntry> entities;
+
+  // Adds a new entity to this vector of entities, and returns its
+  // ID. Can only support 2^64 entities without ID conflicts.
+  EntityID NewEntity()
+  {
+    // std::vector::size runs in constant time.
+    entities.push_back({ entities.size(), ComponentMask() });
+    return entities.back().id;
+  }
+
+  // Assigns the entity associated with the given entity ID in this
+  // vector of entities the given component.
+  template<typename T>
+  void Assign(EntityID id)
+  {
+    int componentId = GetId<T>();
+    entities[id].mask.set(componentId);
+  }
+};
 
 int main() {
-    Scene* my_scene = new Scene();
-    EntityID my_entity = my_scene->NewEntity();
-    TransformComponent* transform = my_scene->AssignNewComponent<TransformComponent>(my_entity);
-    // I'm not smart enough to know if this design sucks :3
-    transform = MoveEntity(transform);
+  Scene scene;
+
+  EntityID newEnt = scene.NewEntity();
+  scene.Assign<TransformComponent>(newEnt);
 }
