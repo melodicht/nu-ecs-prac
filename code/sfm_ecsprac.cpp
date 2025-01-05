7c5370fe334763c5239675f4db58e80a3fb0a0b9
melodicht <marvincht@gmail.com>
2025-01-04 20:57:30 -0800

    Add FPS counter

--- code/sfm_ecsprac.cpp
+++ code/sfm_ecsprac.cpp
@@ -10,6 +10,8 @@
 #define u8  uint8_t
 #define u32 uint32_t
 #define u64 uint64_t
+typedef float f32;
+typedef double f64;
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
@@ -17,12 +19,17 @@
 #include "ecs.cpp"
 #include "ecsprac.cpp"
 
+#include "platform_metrics.cpp"
+
 int main() {
   srand (static_cast <unsigned> (time(0)));
+	u64 cpuTimerFreq = EstimateCPUTimerFreq();
   sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "BALLS!");
   Scene scene;
 
   GameInitialize(scene);
+
+	u64 lastCounter = ReadCPUTimer();
   
   // run the program as long as the window is open
   while (window.isOpen())
@@ -41,6 +48,14 @@ int main() {
 		GameUpdateAndRender(scene, window);
     
     window.display();
+
+		u64 endCounter = ReadCPUTimer();
+
+		u64 counterElapsed = endCounter - lastCounter;
+		f32 msPerFrame = 1000.0f * (f32)counterElapsed/(f32)cpuTimerFreq;
+		f32 fps = (f32)cpuTimerFreq/(f32)counterElapsed;
+		printf("%.02f ms/frame (FPS: %.02f)\n", msPerFrame, fps);
+		lastCounter = endCounter;
   }
 
   return 0;
