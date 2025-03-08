592c4e0aaf36897f1b421d7555de5220e58460f7
melodicht <marvincht@gmail.com>
2025-03-08 13:46:47 -0500

    Update TODO comment for gen id (it sucks)

--- code/imgui.h
+++ code/imgui.h
@@ -2,10 +2,11 @@
 
 // Inspired by https://solhsa.com/imgui/index.html
 
-// If you're going to render widgets to the same
-// UI from different source files, you can avoid
-// ID collisions by defining IMGUI_SRC_ID before
-// this define block:
+// TODO(imguigenid): This doesn't work if making widgets in a for
+// loop, which isn't inconceivable!
+// If you're going to render widgets to the same UI from different
+// source files, you can avoid ID collisions by defining IMGUI_SRC_ID
+// before this define block:
 #ifdef IMGUI_SRC_ID
 #define GEN_ID ((IMGUI_SRC_ID) + (__LINE__))
 #else
