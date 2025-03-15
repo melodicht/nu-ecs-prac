8cd0ec5795221fa03ebf0351eee41215fe168f0e
melodicht <marvincht@gmail.com>
2025-03-15 13:55:44 -0400

    Fix font loading, but text doesn't work

--- code/draw_utils.cpp
+++ code/draw_utils.cpp
@@ -13,9 +13,36 @@ void DrawRect(sf::RenderWindow &window, u32 topLeftX, u32 topLeftY, u32 w, u32 h
 	window.draw(rect);
 }
 
-sf::Font font("arial.ttf");
 // Draws the given text on the window at the given top-left 2D coordinate.
 void DrawText(sf::RenderWindow &window, char *text, u32 topLeftX, u32 topLeftY)
 {
 	// TODO: Fill in this.
+	sf::Font font;
+	if (!font.loadFromFile("main.ttf"))
+	{
+    // error...
+	}
+	else
+	{
+		/*
+		sf::Text text(font); 
+		// set the string to display
+		text.setString("Hello world");
+
+		// set the character size
+		text.setCharacterSize(24); // in pixels, not points!
+
+		// set the color
+		text.setFillColor(sf::Color::Red);
+
+		// set the text style
+		text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+
+		...
+
+			// inside the main loop, between window.clear() and window.display()
+			window.draw(text);
+		*/
+	}
+	
 }
