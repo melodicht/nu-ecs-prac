a5e9d82333fb04d70a5d54cbd5569b788e7a8109
Jianwen Ding <jianwen_ding@icloud.com>
2025-05-18 17:03:55 -0400

    Got emscripten to compile

--- src/renderer/wpu_backend/renderer_wpu.h
+++ src/renderer/wpu_backend/renderer_wpu.h
@@ -1,41 +1,51 @@
 #pragma once
 
-#include "render_backend.h"
+#include "renderer/render_types.h"
+#include "math/math_consts.h"
+#include "asset_types.h"
+
+#include <SDL3/SDL.h>
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-// Currently is a bit of a dummy class 
-class WPURenderBackend : public IRenderBackend {
+#include <cstdint>
+
+// Allows for encapsulation of WebGPU render capabilities
+class WPURenderBackend {
 public:
-    WPURenderBackend() {}
+    WPURenderBackend() { }
+
+    // Gets the SDL Flags eneded
+    SDL_WindowFlags GetRenderWindowFlags() { return 0; }
 
     // Sets a SDL window to draw to and initializes the back end
-    virtual void InitRenderer(SDL_Window *window) override;
+    void InitRenderer(SDL_Window *window, u32 startWidth, u32 startHeight);
 
     // Moves mesh to the GPU, 
     // Returns a uint that represents the mesh's ID
-    virtual uint32_t UploadMesh(uint32_t vertCount, Vertex* vertices, uint32_t indexCount, uint32_t* indices) override { return 0; };
-    virtual uint32_t UploadMesh(MeshAsset &asset) override { return 0; };
+    uint32_t UploadMesh(uint32_t vertCount, Vertex* vertices, uint32_t indexCount, uint32_t* indices) { return 0; };
+    uint32_t UploadMesh(MeshAsset &asset) { return 0; }
 
     // Takes in a mesh ID and represents
-    virtual void DestroyMesh(uint32_t meshID) override { };
+    void DestroyMesh(uint32_t meshID) { }
 
     // Establishes that the following commands apply to a new frame
-    virtual bool InitFrame() override { return false; };
+    bool InitFrame() { return false; }
 
     // Sets the view of a camera
-    virtual void SetCamera(glm::mat4 view, glm::mat4 proj, glm::vec3 pos) override { };
+    void SetCamera(glm::mat4 view, glm::mat4 proj, glm::vec3 pos) { }
 
     // Sets the mesh currently being rendered to
-    virtual void SetMesh(uint32_t meshID) override { };
+    void SetMesh(uint32_t meshID) { }
 
     // Send the matrices of the models to render (Must be called between InitFrame and EndFrame)
-    virtual void SendObjectData(std::vector<ObjectData>& objects) override { };
+    void SendObjectData(std::vector<ObjectData>& objects) { }
 
     // End the frame and present it to the screen
-    virtual void EndFrame() override { };
+    void EndFrame() { }
 
     // Draw multiple objects to the screen (Must be called between InitFrame and EndFrame and after SetMesh)
-    virtual void DrawObjects(int count, int startIndex) override { };
+    void DrawObjects(int count, int startIndex) { }
 };
