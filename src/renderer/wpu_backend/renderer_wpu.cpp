5ead3d31e135a2e5085f20a7d6446355701d488b
Jianwen Ding <jianwen_ding@icloud.com>
2025-05-20 21:34:15 -0400

    Connected emdawnwebgpu and finished porting in device

--- src/renderer/wpu_backend/renderer_wpu.cpp
+++ src/renderer/wpu_backend/renderer_wpu.cpp
@@ -23,15 +23,7 @@ WGPUAdapter requestAdapterSync(WGPUInstance instance, WGPURequestAdapterOptions 
   WGPUAdapter set = nullptr;
   bool requestEnded = false;
 
-  // Callback called by wgpuInstanceRequestAdapter when the request returns
-  // This is a C++ lambda function, but could be any function defined in the
-  // global scope. It must be non-capturing (the brackets [] are empty) so
-  // that it behaves like a regular C function pointer, which is what
-  // wgpuInstanceRequestAdapter expects (WebGPU being a C API). The workaround
-  // is to convey what we want to capture through the pUserData pointer,
-  // provided as the last argument of wgpuInstanceRequestAdapter and received
-  // by the callback as its last argument.
-  WGPURequestAdapterCallbackInfo callbackInfo = WGPURequestAdapterCallbackInfo();
+  WGPURequestAdapterCallbackInfo callbackInfo;
   callbackInfo.callback = [](WGPURequestAdapterStatus status, WGPUAdapterImpl* adapter, WGPUStringView message, void *userdata1, void *userdata2) 
   {
       if (status == WGPURequestAdapterStatus_Success) {
@@ -55,7 +47,7 @@ WGPUAdapter requestAdapterSync(WGPUInstance instance, WGPURequestAdapterOptions 
 
   // We wait until userData.requestEnded gets true
   #if EMSCRIPTEN
-  while (!userData.requestEnded) {
+  while (!requestEnded) {
       emscripten_sleep(100);
   }
   #endif
@@ -65,6 +57,66 @@ WGPUAdapter requestAdapterSync(WGPUInstance instance, WGPURequestAdapterOptions 
   return set;
 }
 
+WGPUDevice requestDeviceSync(WGPUAdapter adapter, WGPUDeviceDescriptor const * descriptor) {
+  WGPUDevice set = nullptr;
+  bool requestEnded = false;
+
+  WGPURequestDeviceCallbackInfo callbackInfo;
+  callbackInfo.callback = [](WGPURequestDeviceStatus status, WGPUDevice device, WGPUStringView message, void * userdata1, void * userdata2) {
+      if (status == WGPURequestDeviceStatus_Success) {
+        *((WGPUDevice *)userdata1) = device;
+      } else {
+          std::cout << "Could not get WebGPU device: " << message.data << std::endl;
+      }
+      *((bool *)userdata2) = true;
+  };
+  callbackInfo.mode = WGPUCallbackMode_AllowSpontaneous;
+  callbackInfo.userdata1 = &set;
+  callbackInfo.userdata2 = &requestEnded;
+
+  wgpuAdapterRequestDevice(
+      adapter,
+      descriptor,
+      callbackInfo
+  );
+
+#ifdef __EMSCRIPTEN__
+  while (!requestEnded) {
+      emscripten_sleep(100);
+  }
+#endif // __EMSCRIPTEN__
+
+  assert(requestEnded);
+
+  return set;
+}
+
+// We also add an inspect device function:
+void inspectDevice(WGPUDevice device) {
+  WGPUSupportedFeatures features;
+  wgpuDeviceGetFeatures(device, &features);
+
+  std::cout << "Device features:" << std::endl;
+  std::cout << std::hex;
+  for (int iter = 0; iter < features.featureCount ; iter++) {
+      std::cout << " - 0x" << features.features[iter] << std::endl;
+  }
+  std::cout << std::dec;
+
+  WGPULimits limits = {};
+  limits.nextInChain = nullptr;
+
+  WGPUStatus success = wgpuDeviceGetLimits(device, &limits);
+
+  if (success == WGPUStatus_Success) {
+      std::cout << "Device limits:" << std::endl;
+      std::cout << " - maxTextureDimension1D: " << limits.maxTextureDimension1D << std::endl;
+      std::cout << " - maxTextureDimension2D: " << limits.maxTextureDimension2D << std::endl;
+      std::cout << " - maxTextureDimension3D: " << limits.maxTextureDimension3D << std::endl;
+      std::cout << " - maxTextureArrayLayers: " << limits.maxTextureArrayLayers << std::endl;
+      // [...] Extra device limits
+  }
+}
 // Much of this was taken from https://eliemichel.github.io/LearnWebGPU
 void WPURenderBackend::InitRenderer(SDL_Window *window, u32 startWidth, u32 startHeight) {
   // Creates instance
@@ -89,4 +141,49 @@ void WPURenderBackend::InitRenderer(SDL_Window *window, u32 startWidth, u32 star
   WGPUAdapter adapter = requestAdapterSync(instance, &adapterOpts);
 
   std::cout << "Got adapter: " << adapter << std::endl;
+
+  std::cout << "Requesting device..." << std::endl;
+
+  WGPUDeviceDescriptor deviceDesc = {};
+
+  // General device description
+  deviceDesc.nextInChain = nullptr;
+  deviceDesc.label = WGPUStringView{
+    .data = "My Device",
+    .length = 9}; // anything works here, that's your call
+  deviceDesc.requiredFeatureCount = 0; // we do not require any specific feature
+  deviceDesc.requiredLimits = nullptr; // we do not require any specific limit
+  deviceDesc.defaultQueue.nextInChain = nullptr;
+  deviceDesc.defaultQueue.label = WGPUStringView{
+    .data = "Default Queue",
+    .length = 13};
+
+  // Lost device callback
+  WGPUDeviceLostCallbackInfo lostCallbackInfo;
+  lostCallbackInfo.callback = [](WGPUDevice const * device, WGPUDeviceLostReason reason, WGPUStringView message, WGPU_NULLABLE void* userdata1, WGPU_NULLABLE void* userdata2) {
+    std::cout << "Device lost: reason " << reason;
+    if (message.data) std::cout << " (" << message.data << ")";
+    std::cout << std::endl;
+  };
+  lostCallbackInfo.mode = WGPUCallbackMode_AllowSpontaneous;
+  deviceDesc.deviceLostCallbackInfo = lostCallbackInfo;
+
+  // Error callback
+  WGPUUncapturedErrorCallbackInfo lostErrorInfo;
+  lostErrorInfo.callback = [](WGPUDevice const * device, WGPUErrorType type, WGPUStringView message, WGPU_NULLABLE void* userdata1, WGPU_NULLABLE void* userdata2) {
+    std::cout << "Error happened: error " << type;
+    if (message.data) std::cout << " (" << message.data << ")";
+    std::cout << std::endl;
+  };
+  deviceDesc.uncapturedErrorCallbackInfo = lostErrorInfo;
+
+  WGPUDevice device = requestDeviceSync(adapter, &deviceDesc);
+
+  std::cout << "Got device: " << device << std::endl;
+
+  wgpuAdapterRelease(adapter);
+
+  inspectDevice(device);
+
+  wgpuDeviceRelease(device);
 }
