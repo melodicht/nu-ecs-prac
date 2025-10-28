b659c9bd6a298cf5d622f3231d558f8d1b0fccb9
Enator18 <regina.e@northeastern.edu>
2025-10-28 19:24:59 -0400

    removed hardcoded entity spawning for the city demo since it uses the scene loader now

--- src/game.cpp
+++ src/game.cpp
@@ -116,47 +116,6 @@ GAME_INITIALIZE(GameInitialize)
 //    JPH::Quat characterRotation = JPH::Quat(0, 0, 0, 0);
 //    JPH::CharacterVirtual *characterVirtual = new JPH::CharacterVirtual(&characterVirtualSettings, characterPosition, characterRotation, physicsSystem);
 //    playerCharacter->characterVirtual = characterVirtual;
-//
-//    // NOTE(marvin): This is the freecam player, not to be confused with the player character.
-//    // Probably should give it a different name but I am lazy.
-//    EntityID player = scene.NewEntity();
-//    Transform3D *playerTransform = scene.Assign<Transform3D>(player);
-//    CameraComponent *playerCamera = scene.Assign<CameraComponent>(player);
-//    FlyingMovement *playerMovement = scene.Assign<FlyingMovement>(player);
-//    playerCamera->fov = 90.0f;
-//    playerCamera->farPlane = 5000.0f;
-//    playerCamera->nearPlane = 0.2f;
-//    playerMovement->moveSpeed = 200.0f;
-//    playerMovement->turnSpeed = 0.1f;
-//    playerTransform->position.x = -2560.0f;
-//    playerTransform->position.z = 1536.0f;
-//    playerTransform->rotation.y = 30.0f;
-//
-//    EntityID startPlane = scene.NewEntity();
-//    scene.Assign<Transform3D>(startPlane);
-//    Plane *planeSize = scene.Assign<Plane>(startPlane);
-//    planeSize->width = 4096.0f;
-//    planeSize->length = 4096.0f;
-//
-//    EntityID startCube = scene.NewEntity();
-//    Transform3D* cubeTransform = scene.Assign<Transform3D>(startCube);
-//    cubeTransform->scale.x = 4096.0f;
-//    cubeTransform->scale.y = 4096.0f;
-//    cubeTransform->scale.z = 16.0f;
-//    cubeTransform->position.z = -8.0f;
-//    MeshComponent* startCubeMesh = scene.Assign<MeshComponent>(startCube);
-//    startCubeMesh->mesh = cuboidMesh;
-//    f32 shade = RandInBetween(0.25f, 0.75f);
-//    startCubeMesh->r = shade;
-//    startCubeMesh->g = shade;
-//    startCubeMesh->b = shade;
-//
-//    EntityID directionalLight = scene.NewEntity();
-//    Transform3D* lightTransform = scene.Assign<Transform3D>(directionalLight);
-//    lightTransform->rotation = {0, 30, 120};
-//    DirLight* dirLight = scene.Assign<DirLight>(directionalLight);
-//    dirLight->diffuse = {0.2, 0.2, 0.6};
-//    dirLight->specular = {0.4, 0.4, 0.5};
 
 
     scene.InitSystems();
